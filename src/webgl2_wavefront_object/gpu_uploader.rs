5874fcb79b3e90d894e5cca9752c1de3d30d4ca1
Stampatron <james@nitori.local>
2026-04-10 10:53:31 +0900

    Renamed object types and added logic checking to known how to parse and buffer each type. Still does not do anything special with diffuse models.

--- src/webgl2_wavefront_object/gpu_uploader.rs
+++ src/webgl2_wavefront_object/gpu_uploader.rs
@@ -137,7 +137,102 @@ impl WebGl2WavefrontObject
         return Ok(());
     }
 
-    pub(in super) fn no_materials_object_gpu_buffer(&mut self, context: &WebGl2RenderingContext, program: &Option<WebGlProgram>) -> Result<(), String>
+    pub(in super) fn unlit_object_gpu_buffer(&mut self, context: &WebGl2RenderingContext, program: &Option<WebGlProgram>) -> Result<(), String>
+    {
+        /*
+            get the information from the object in a workable format
+        */
+        let vertex_positions: Vec<f32> = self.get_vertex_positions();
+		rust_log(&format!("Vertices array is size: {}", vertex_positions.len()), &"verbose_wasm_parse");
+		let vertex_indices: Vec<u16> = self.get_vertex_indices();
+		rust_log(&format!("Vertex indices array is size: {}", vertex_indices.len()), &"verbose_wasm_parse");
+
+        /*
+            Manage Vertices for model
+        
+            Note that `Float32Array::view` is somewhat dangerous (hence the
+            `unsafe`!). This is creating a raw view into our module's
+            `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+			(aka do a memory allocation in Rust) it'll cause the buffer to change,
+			causing the `Float32Array` to be invalid.
+				
+			As a result, after `Float32Array::view` we have to be very careful not to
+			do any memory allocations before it's dropped.
+        */
+        unsafe 
+        {
+            rust_log(&"Starting to buffer vertex locations...", &"verbose_wasm_gpu_data");
+            self.vertex_buffer = context.create_buffer();
+            context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, self.vertex_buffer.as_ref());
+
+            let position_attribute_location = context.get_attrib_location(program.as_ref().unwrap(), "a_position") as u32;
+            context.vertex_attrib_pointer_with_i32(position_attribute_location, 3, WebGl2RenderingContext::FLOAT, false, 0, 0);	
+                    
+            let vert_array = js_sys::Float32Array::view(&vertex_positions);
+            context.buffer_data_with_array_buffer_view
+            (
+                WebGl2RenderingContext::ARRAY_BUFFER,
+                &vert_array,
+                WebGl2RenderingContext::STATIC_DRAW,
+            );
+            context.enable_vertex_attrib_array(position_attribute_location);
+            rust_log(&"...buffering complete.", &"verbose_wasm_gpu_data");
+        }
+
+        /*
+            Manage Colors for model
+        */
+        unsafe {
+            rust_log(&"Starting to buffer color data...", &"verbose_wasm_gpu_data");
+            self.color_buffer = context.create_buffer();
+            context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, self.color_buffer.as_ref());
+            let color_attribute_location = context.get_attrib_location(program.as_ref().unwrap(), "a_color") as u32;
+            context.vertex_attrib_pointer_with_i32(color_attribute_location, 4, WebGl2RenderingContext::FLOAT, false, 0, 0);
+            context.enable_vertex_attrib_array(color_attribute_location);
+    
+            //Currently junk colors. Only care about matching vertex count in sample cube
+            let mut rng = rand::rng();
+            let mut colors: Vec<f32> = Vec::new();
+            for _ in 0..self.obj.vertices.len()
+            {
+                colors.push(rng.random_range(0.0..=1.0));
+                colors.push(rng.random_range(0.0..=1.0));
+                colors.push(rng.random_range(0.0..=1.0));
+                colors.push(1.0);
+            }
+            let color_array = js_sys::Float32Array::view(&colors);
+            context.buffer_data_with_array_buffer_view
+            (
+                WebGl2RenderingContext::ARRAY_BUFFER,
+                &color_array,
+                WebGl2RenderingContext::STATIC_DRAW,
+            );
+            rust_log(&"...buffering complete.", &"verbose_wasm_gpu_data");
+        }
+
+        /*
+            Manage Indices for model
+        */
+        rust_log(&"Starting to buffer vertex indices...", &"verbose_wasm_gpu_data");
+        self.vertex_index_buffer = context.create_buffer();
+        context.bind_buffer(WebGl2RenderingContext::ELEMENT_ARRAY_BUFFER, self.vertex_index_buffer.as_ref());
+        unsafe 
+        {
+            let tmp_indices = self.get_vertex_indices();
+            self.indices_size = tmp_indices.len();	 
+            let converted_indices = js_sys::Uint16Array::view(&tmp_indices);
+            context.buffer_data_with_array_buffer_view(
+                WebGl2RenderingContext::ELEMENT_ARRAY_BUFFER,
+                &converted_indices,
+                WebGl2RenderingContext::STATIC_DRAW,
+            );
+        }
+        rust_log(&"...buffering complete.", &"verbose_wasm_gpu_data");
+        
+        return Ok(());
+    }
+
+    pub(in super) fn diffuse_object_gpu_buffer(&mut self, context: &WebGl2RenderingContext, program: &Option<WebGlProgram>) -> Result<(), String>
     {
         /*
             get the information from the object in a workable format
